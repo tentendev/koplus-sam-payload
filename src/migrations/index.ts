0cc46e07d4e7af5cd06a8c7071a16fa48a014e28
tentendev <dev@tenten.co>
2026-07-23 17:18:32 +0800

    Revert "Add production migration for Clerk user fields"
    This reverts commit 18b30e6ab4696550b484efeb59eb17e194dc9f92.

--- src/migrations/index.ts
+++
@@ -1,9 +0,0 @@
-import * as addClerkUserFields from './20260723_091500_add_clerk_user_fields'
-
-export const migrations = [
-  {
-    name: addClerkUserFields.name,
-    up: addClerkUserFields.up,
-    down: addClerkUserFields.down,
-  },
-]
--- src/payload.config.ts
+++ src/payload.config.ts
@@ -14,7 +14,6 @@ import { Colors } from './collections/Colors'
 import { Accessories } from './collections/Accessories'
 import { Products } from './collections/Products'
 import { QuoteRequests } from './collections/QuoteRequests'
-import { migrations } from './migrations'
 
 const filename = fileURLToPath(import.meta.url)
 const dirname = path.dirname(filename)
@@ -95,7 +94,6 @@ export default buildConfig({
         pool: {
           connectionString: process.env.DATABASE_URI as string,
         },
-        prodMigrations: migrations,
       })
     : sqliteAdapter({
         client: {
